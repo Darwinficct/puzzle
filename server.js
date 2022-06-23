fbc8465d4c640cf1b2522c0f80c9785f79e2a901
Darwinficct <darwinag73@gmail.com>
2022-06-23 16:04:00 -0400

    make it better

--- server.js
+++ server.js
@@ -3,12 +3,19 @@ const express = require('express');
 const app = express();
 
 
-const server = require('http').createServer(app);
+app.set('port', process.env.PORT || 3000);
 
+//static files
+ app.use(express.static(path.join(__dirname, 'public')));
 
-const io = require('socket.io')(server,{
-    cors: {origin: "*"}
+//iniciar servidor
+const server = app.listen(app.get('port'),()=>{
+    console.log('server on port', app.get('port'));
 });
+
+//websocket
+const SocketIO = require('socket.io');
+const io = SocketIO(server);
 
 io.on('connection', (socket) => {
     console.log('connection');
